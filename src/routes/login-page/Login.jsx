6e280fe8bc4838d73305b71ca22f42417dfd1561
ListKelvin <phamm5687@gmai.com>
2022-07-16 13:52:21 +0700

    react-form

--- src/routes/login-page/Login.jsx
+++ src/routes/login-page/Login.jsx
@@ -1,21 +1,30 @@
-import { useState, useContext } from "react";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import "./login.styles.scss";
 import { listAccount } from "../../utils/data";
 import { UserContext } from "../../App";
+import { useForm } from "react-hook-form";
 function Login() {
-  const [admin, setAdmin] = useState("");
-  const [pass, setPass] = useState("");
+  const { register, handleSubmit, getValues, resetField } = useForm();
+  // const onSubmit = (data) => console.log(watch("username"));
+
+  // const [admin, setAdmin] = useState("");
+  // const [pass, setPass] = useState("");
   const { setUser } = useContext(UserContext);
   let history = useNavigate();
   const handelLogin = () => {
     let account = listAccount.filter(function (account) {
-      return account.username === admin && account.password === pass;
+      return (
+        account.username === getValues("username") &&
+        account.password === getValues("password")
+      );
     });
-    if (account.length === 0) {
+    const userName = getValues("username");
+    const passWord = getValues("password");
+    if (account.length === 0 || userName === "" || passWord === "") {
       alert("The account does not exist!!");
-      setAdmin("");
-      setPass("");
+      resetField("username");
+      resetField("password");
       return;
     } else {
       setUser({ loggedIn: true });
@@ -26,23 +35,23 @@ function Login() {
   return (
     <div>
       <div className="login-container">
-        <div className="form">
+        <form className="form" onSubmit={handleSubmit(handelLogin)}>
           <p className="title align">username</p>
           <input
             className="align"
-            value={admin}
-            onChange={(event) => setAdmin(event.target.value)}
+            {...register("username")}
+            // value={admin}
+            // onChange={(event) => setAdmin(event.target.value)}
           />
           <p className="title align">password</p>
           <input
             className="align"
-            value={pass}
-            onChange={(event) => setPass(event.target.value)}
+            {...register("password")}
+            // value={pass}
+            // onChange={(event) => setPass(event.target.value)}
           />
-          <button className="btn align" onClick={handelLogin}>
-            submit
-          </button>
-        </div>
+          <button className="btn align">submit</button>
+        </form>
       </div>
     </div>
   );
